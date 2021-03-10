447e923ba1ae02c398d95b7005feee5170b43567
Artem Klishch <artemklishch@gmail.com>
2021-03-10 17:00:47 +0200

    fgdssdfsdf

--- 6debug/pr1/src/App.js
+++ 6debug/pr1/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.css";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -39,13 +40,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                key={person.id}
-                name={person.name}
-                age={person.age}
-                click={this.deletePerson.bind(this, index)}
-                changed={(e) => this.nameChangedHandler(e, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={this.deletePerson.bind(this, index)}
+                  changed={(e) => this.nameChangedHandler(e, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
