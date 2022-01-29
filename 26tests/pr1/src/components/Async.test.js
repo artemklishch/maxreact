2b296b22e0224bf67cb2b13ee3b77421a9960131
Artem Klishch <artemklishch@gmail.com>
2022-01-29 23:22:56 +0200

    dfsgdsfgdf

--- 26tests/pr1/src/components/Async.test.js
+++ 26tests/pr1/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders posts if succeed", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
     const listItemsElements = await screen.findAllByRole("listitem");
     expect(listItemsElements).not.toHaveLength(0);
